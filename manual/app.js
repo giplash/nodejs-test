b26b196ad48a8335998d36b195e3c27d75d0d27e
giplash <anashkinviktar@gmail.com>
2018-05-21 00:59:10 +0300

    initial commit

---
+++ manual/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use('/public', express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.get('/pasha', (req, res) => {
+  res.render('petuh');
+})
+
+app.get('/id/:id', (req, res) => {
+  res.render('id', {id: req.params.id});
+})
+
+app.listen(3000);
